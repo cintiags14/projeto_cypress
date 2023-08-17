fad643b42b71fb279cd9863cc6ef2cbcd0b2e844
cintiags14 <cintiags14@hotmail.com>
2023-08-17 15:35:13 -0300

    arquivo elements

--- cypress/support/pages/acesso a pagina/elemets.js
+++ cypress/support/pages/acesso a pagina/elemets.js
@@ -1,5 +1,5 @@
 
 
 export const ELEMENTS ={
-    idproduct: '#dropdownProduct'
+    idclas: 'textarea.gLFyf#APjFqb'
 }
--- cypress/support/pages/acesso a pagina/index.js
+++ cypress/support/pages/acesso a pagina/index.js
@@ -5,11 +5,11 @@ const el = require('./elemets').ELEMENTS;
 class acesso{
     acessarPagina(){
     cy.visit('https://www.google.com.br/?hl=pt-BR');
-    cy.get('textarea.gLFyf#APjFqb').type('cypress.io{enter}');
+    cy.get(el.idclas).type('cypress.io{enter}');
     cy.contains('https://www.cypress.io').should('be.visible')
     .click();
     cy.origin('https://www.cypress.io', () => {
-    cy.get(el.idproduct).trigger('mouseover')
+    cy.get('#dropdownProduct').trigger('mouseover')
     cy.contains('Browser Testing').click({force: true});
     })
      }
